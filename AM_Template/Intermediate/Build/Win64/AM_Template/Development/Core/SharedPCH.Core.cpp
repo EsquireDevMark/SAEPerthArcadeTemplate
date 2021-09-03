9a435219b82ee9e02be31b1d1d9cf7a3a6f1aa71
Mark <mark.thompson@hotmail.com.au>
2021-09-03 15:45:11 +0800

    Fix Missing files

---
+++ AM_Template/Intermediate/Build/Win64/AM_Template/Development/Core/SharedPCH.Core.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29336
+#include "C:/Users/mpthompson/Desktop/ArcadeTemplate/AM_Template/Intermediate/Build/Win64/AM_Template/Development/Core/SharedPCH.Core.h"
---
+++ AM_Template/Intermediate/Build/Win64/AM_Template/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29913
+#include "C:/Users/mpthompson/Desktop/AM_Template/Intermediate/Build/Win64/AM_Template/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
