b5291c955ee914d9d1a6907e00ef1eb03a590415
Mark <mark.thompson@hotmail.com.au>
2021-09-06 14:58:06 +0800

    Added DLL support

--- AM_Template/Intermediate/Build/Win64/AM_TemplateEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ AM_Template/Intermediate/Build/Win64/AM_TemplateEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.28.29914
-#include "C:/Users/markt/Desktop/AM_Template/Intermediate/Build/Win64/AM_TemplateEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.28.29913
+#include "C:/Users/mpthompson/Desktop/SAEPerthArcadeTemplate/AM_Template/Intermediate/Build/Win64/AM_TemplateEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
